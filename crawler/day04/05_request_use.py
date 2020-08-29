a5da6ecaaf0f4cd7f7922fdfe6785a975e785b4e
teng <wwwsousoucom@126.com>
2020-08-29 12:00:33 +0800

    request cookie

--- crawler/day04/05_request_use.py
+++ crawler/day04/05_request_use.py
@@ -10,26 +10,20 @@ class RequestSpider(object):
         self.response = requests.get(url, headers=headers)
 
     def run(self):
-
         data = self.response.content
-
         # 1.获取请求头
         request_headers = self.response.request.headers
-
         # 2.获取相应头
         coderesponse_headers = self.response.headers
-
         # 3.响应状态码
         code = self.response.status_code
-
-
         # 4. 请求的cookie
         request_cookie = self.response.request._cookies
         print(request_cookie)
         # 5. 响应的cookie
         response_cookie = self.response.cookies
         print(response_cookie)
-        
+
 
 if __name__ == '__main__':
     RequestSpider().run()
